fe9104fe8e4538b552730ebb14b1b1b822056cf4
Luferov Victor <lyferov@yandex.ru>
2022-04-25 15:31:35 +0300

    fix: Исправлена проблема с загрузкой КБК

--- devind_dictionaries/management/commands/load_budget_classification.py
+++ devind_dictionaries/management/commands/load_budget_classification.py
@@ -1,10 +1,9 @@
 """Module with provide command for upload data to database."""
 
 import json
-from os.path import join
+from os.path import dirname, join
 
 from django.apps import apps
-from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from django.db.models import Model
@@ -18,10 +17,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options) -> None:
         """Invoke command."""
         model: Model = apps.get_model('devind_dictionaries', 'BudgetClassification')
+        dir_name: str = dirname(dirname(__file__))
         file: str = join(
-            settings.BASE_DIR,
-            'devind_dictionaries',
-            'management',
+            dir_name,
             'seed',
             '001.devind_dictionaries',
             '001.BudgetClassification.json'
