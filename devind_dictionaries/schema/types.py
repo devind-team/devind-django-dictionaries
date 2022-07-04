f8e77f4796d7639e32b7780ad288dd87575eb969
MarionetkaX <sergey07061999@gmail.com>
2022-07-04 17:54:05 +0300

    feat: переписаны типы для strawberry_django_plus

--- devind_dictionaries/schema/types.py
+++ devind_dictionaries/schema/types.py
@@ -4,7 +4,7 @@ import datetime
 from typing import Optional
 import strawberry
 from strawberry import auto
-import strawberry_django
+from strawberry_django_plus import gql
 from devind_dictionaries.models import (
     BudgetClassification,
     Department,
@@ -20,7 +20,7 @@ class BaseTimeStamps:
     updated_at: datetime.datetime
 
 
-@strawberry_django.type(BudgetClassification)
+@gql.django.type(BudgetClassification)
 class BudgetClassificationType(BaseTimeStamps):
     """Type of Budget classification."""
 
@@ -33,7 +33,20 @@ class BudgetClassificationType(BaseTimeStamps):
     end: datetime.datetime
 
 
-@strawberry_django.type(District)
+@gql.django.filter(BudgetClassification)
+class BudgetClassificationFilter(BaseTimeStamps):
+    """Filter of Budget classification."""
+
+    id: auto
+    code: str
+    name: str
+
+    active: bool
+    start: datetime.datetime
+    end: datetime.datetime
+
+
+@gql.django.type(District)
 class DistrictType(BaseTimeStamps):
     """Type of District."""
 
@@ -41,7 +54,7 @@ class DistrictType(BaseTimeStamps):
     name: str
 
 
-@strawberry_django.type(Region)
+@gql.django.type(Region)
 class RegionType(BaseTimeStamps):
     """Type of Region."""
 
@@ -52,7 +65,7 @@ class RegionType(BaseTimeStamps):
     district: DistrictType
 
 
-@strawberry_django.type(Organization)
+@gql.django.type(Organization)
 class OrganizationType(BaseTimeStamps):
     """Type of Organization."""
 
@@ -82,7 +95,37 @@ class OrganizationType(BaseTimeStamps):
     # users: list['UserType']
 
 
-@strawberry_django.type(Department)
+@gql.django.partial(Organization)
+class OrganizationInputPartial(BaseTimeStamps, gql.NodeInput):
+    """Partial of Organization."""
+
+    id: auto
+    name: str
+    present_name: str
+
+    inn: Optional[str]
+    kpp: Optional[str]
+    kind: Optional[str]
+
+    rubpnubp: Optional[str]
+    kodbuhg: Optional[str]
+    okpo: Optional[str]
+
+    phone: Optional[str]
+    site: Optional[str]
+    mail: Optional[str]
+    address: Optional[str]
+
+    attributes: strawberry.scalars.JSON
+
+    parent: Optional['OrganizationType']
+    region: Optional[RegionType]
+
+    # user: 'UserType'
+    # users: list['UserType']
+
+
+@gql.django.type(Department)
 class DepartmentType(BaseTimeStamps):
     """Type of Departments."""
 
