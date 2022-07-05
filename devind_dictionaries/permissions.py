8c3a7a78641b5d4b83c0d650edc3a56325c18397
MarionetkaX <sergey07061999@gmail.com>
2022-07-05 12:24:11 +0300

    feat: переписаны пермишены

--- devind_dictionaries/permissions.py
+++ devind_dictionaries/permissions.py
@@ -1,8 +1,15 @@
 """Module define permission in dictionaries."""
 
-from devind_helpers.permissions import ModelPermission
+from strawberry_django_plus.permissions import HasPerm
 
 
-ChangeDistrict = ModelPermission('devind_dictionaries.change_district')
-ChangeRegion = ModelPermission('devind_dictionaries.change_region')
-ChangeOrganization = ModelPermission('devind_dictionaries.change_organization')
+ChangeDistrict = HasPerm('devind_dictionaries.change_district')
+ChangeRegion = HasPerm('devind_dictionaries.change_region')
+ChangeOrganization = HasPerm('devind_dictionaries.change_organization')
+
+# from devind_helpers.permissions import ModelPermission
+#
+#
+# ChangeDistrict = ModelPermission('devind_dictionaries.change_district')
+# ChangeRegion = ModelPermission('devind_dictionaries.change_region')
+# ChangeOrganization = ModelPermission('devind_dictionaries.change_organization')
